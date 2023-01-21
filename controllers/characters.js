576fab07799a313ac2386bbf417f504078ae0806
swordgeo <swordgeo1094@yahoo.com>
2023-01-21 14:04:56 -0500

    Finish lesson 6

--- controllers/characters.js
+++ controllers/characters.js
@@ -4,8 +4,10 @@ const Character = require('../models/character')
 const getCharacters = async (req, res, next) => {
   try {
     const characters = await Character.find();
-    res.json(characters);
+    //200 is generic success
+    res.status(200).json(characters);
   } catch (err) {
+      //500 means server error, not user error
     res.status(500).json({message: err.message});
   }
 };
@@ -14,38 +16,34 @@ const getCharacters = async (req, res, next) => {
 const getCharacter = async (req, res, next) => {
   try {
     const character = await Character.findById(req.params.id);
-    res.json(character);
+    res.status(200).json(character);
   } catch (err) {
+    //500 means server error, not user error
     res.status(500).json({message: err.message});
   }
 };
 
 
 const addCharacter = async (req, res, next) => {
-  //I notice it'll let me have some of these values blank but we'll deal with that in time
-  const character = new Character({
-    characterName: req.body.characterName,
-    playerName: req.body.playerName,
-    race: req.body.race,
-    class: req.body.class,
-    level: req.body.level,
-    alignment: req.body.alignment,
-    stats: {
-      str: req.body.stats.str,
-      dex: req.body.stats.dex,
-      con: req.body.stats.con,
-      int: req.body.stats.int,
-      wis: req.body.stats.wis,
-      cha: req.body.stats.cha
-    }
-  });
   try {
-    const newCharacter = await character.save();
-    //201 means new thing created as opposed to general 200 "everything worked"
-    res.status(201).json(newCharacter);
+    //Reject if there are any missing fields
+    if (!req.body.characterName || !req.body.playerName || !req.body.race || !req.body.class || !req.body.level || !req.body.alignment || 
+    !req.body.stats.str || !req.body.stats.dex || !req.body.stats.con || !req.body.stats.int || !req.body.stats.wis || !req.body.stats.cha) {
+      //400 means user error - didn't use all values for instance
+      res.status(400).send({ message: 'Content can not be empty!' });
+    }
+
+    const character = new Character(req.body);
+    character.save().then((data) => {
+      //201 means new thing created as opposed to general 200 "everything worked"
+      res.status(201).send(data);
+    })
+    .catch((err) => {
+      res.status(500).json({message: err.message || 'Error occured creating character.'});
+    });
   } catch (err) {
-    //400 means user error - didn't use all values for instance
-    res.status(400).json({message: err.message});
+    //500 means server error, not user error
+    res.status(500).json({message: err.message});
   }
 }
 
@@ -69,13 +67,17 @@ const delCharacter = async (req, res, next) => {
 const editCharacter = async (req, res, next) => {
   try {
     const character = await Character.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    //const character = Character.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    if (!character) return res.status(404).send("No Char found.")
-    res.status(200).send(character)
+    if (!character) {
+      //404 means does not exist
+      return res.status(404).send("No Character found.")
+    }
+    //204 succeeds but doesn't navigate away
+    //The relevant circumstance is I can't run json messages from it
+    //But I'll leave it anyway or else it won't give me any response at all?!
+    res.status(204).send(character);
   } catch(err) {
     res.status(500).send(err)
   }
-
 }
 
 
--- controllers/players.js
+++ controllers/players.js
@@ -1,44 +1,51 @@
 const Player = require('../models/player')
 
-//using try/catch for troubleshooting
 const getPlayers = async (req, res, next) => {
   try {
     const players = await Player.find();
-    res.json(players);
-  } catch (err) {
-    res.status(500).json({message: err.message});
-  }
+      //200 is generic success
+      res.status(200).json(players);
+    } catch (err) {
+        //500 means server error, not user error
+      res.status(500).json({message: err.message});
+    }
 };
 
-//using try/catch for troubleshooting
+
 const getPlayer = async (req, res, next) => { 
   try {
     const player = await Player.findById(req.params.id);
-    res.json(player);
+    res.status(200).json(player);
   } catch (err) {
+    //500 means server error, not user error
     res.status(500).json({message: err.message});
   }
 };
 
-//await mongodb...  .insertOne(player)
+
 const addPlayer = async (req, res, next) => {
-  //I notice it'll let me have some of these values blank but we'll deal with that in time
-  const player = new Player({
-    playerName: req.body.playerName,
-    characterName: req.body.characterName,
-    campaign: req.body.campaign
-  });
   try {
-    const newPlayer = await player.save();
-    //201 means new thing created as opposed to general 200 "everything worked"
-    res.status(201).json(newPlayer);
+    //Reject if there are any missing fields
+    if (!req.body.characterName || !req.body.playerName || !req.body.campaign) {
+      //400 means user error - didn't use all values for instance
+      res.status(400).send({ message: 'Content can not be empty!' });
+    }
+
+    const player = new Player(req.body);
+    player.save().then((data) => {
+      //201 means new thing created as opposed to general 200 "everything worked"
+      res.status(201).send(data);
+    })
+    .catch((err) => {
+      res.status(500).json({message: err.message || 'Error occured creating player.'});
+    });
   } catch (err) {
-    //400 means user error - didn't use all values for instance
-    res.status(400).json({message: err.message});
+    //500 means server error, not user error
+    res.status(500).json({message: err.message});
   }
 }
 
-//using try/catch for troubleshooting
+
 const delPlayer = async (req, res, next) => {
   try {
     const player = await Player.findById(req.params.id);
@@ -54,14 +61,22 @@ const delPlayer = async (req, res, next) => {
   }
 };
 
+
 const editPlayer = async (req, res, next) => {
   try {
     const player = await Player.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    if (!player) return res.status(404).send("No player found.")
-    res.status(200).send(player)
+    if (!player) {
+      //404 means does not exist
+      return res.status(404).send("No player found.")
+    }
+    //204 succeeds but doesn't navigate away
+    //The relevant circumstance is I can't run json messages from it
+    //But I'll leave it anyway or else it won't give me any response at all?!
+    res.status(204).send(player)
   } catch(err) {
     res.status(500).send(err)
   }
 }
+
 
 module.exports = { getPlayers, getPlayer, addPlayer, delPlayer, editPlayer};
